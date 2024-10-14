296688da4f52dde9fe797fb97cf64c0e404a6290
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-10-14 14:30:31 +0500

    Implement TreeMain for demo

---
+++ src/main/java/ru/team/compiler/tree/TreeMain.java
@@ -0,0 +1,44 @@
+package ru.team.compiler.tree;
+
+import ru.team.compiler.token.Token;
+import ru.team.compiler.token.Tokenizer;
+import ru.team.compiler.tree.node.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TreeMain {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter ' to exit, | to parse");
+        StringBuilder stringBuilder = new StringBuilder();
+        while (true) {
+            String code = scanner.nextLine();
+            if (code.strip().equals("'")) {
+                break;
+            }
+
+            if (code.strip().equals("|")) {
+                try {
+                    Tokenizer tokenizer = new Tokenizer(stringBuilder.toString());
+                    List<Token> tokens = new ArrayList<>();
+                    while (tokenizer.hasNext()) {
+                        tokens.add(tokenizer.next());
+                    }
+
+                    TreeNode node = TreeNode.PARSER.parse(tokens);
+                    System.out.println(node);
+                } catch (Exception e) {
+                    System.out.println("ERROR: " + e);
+                }
+                stringBuilder = new StringBuilder();
+
+                continue;
+            }
+
+            stringBuilder.append(code).append("\n");
+        }
+    }
+}
--- src/main/java/ru/team/compiler/tree/node/TreeNode.java
+++ src/main/java/ru/team/compiler/tree/node/TreeNode.java
@@ -1,5 +1,10 @@
 package ru.team.compiler.tree.node;
 
+import org.jetbrains.annotations.NotNull;
+import ru.team.compiler.exception.CompilerException;
+import ru.team.compiler.exception.NodeFormatException;
+import ru.team.compiler.token.Token;
+import ru.team.compiler.token.TokenIterator;
 import ru.team.compiler.tree.node.clas.ClassMemberNode;
 import ru.team.compiler.tree.node.clas.ClassNode;
 import ru.team.compiler.tree.node.clas.ParametersNode;
@@ -11,8 +16,60 @@ import ru.team.compiler.tree.node.primary.PrimaryNode;
 import ru.team.compiler.tree.node.statement.BodyNode;
 import ru.team.compiler.tree.node.statement.StatementNode;
 
+import java.util.List;
+
 public abstract sealed class TreeNode permits PrimaryNode, IdentifierNode, ExpressionNode, ArgumentsNode,
-                                              BodyNode, StatementNode, ParametersNode, ClassMemberNode,
-                                              ClassNode, ProgramNode {
+        BodyNode, StatementNode, ParametersNode, ClassMemberNode,
+        ClassNode, ProgramNode {
+
+    public static final TreeNodeParser<TreeNode> PARSER = new TreeNodeParser<>() {
+
+        private List<TreeNodeParser<? extends TreeNode>> parsers;
+
+        @Override
+        @NotNull
+        public TreeNode parse(@NotNull TokenIterator iterator) throws CompilerException {
+            for (TreeNodeParser<? extends TreeNode> parser : parsers()) {
+                TokenIterator copiedIterator = iterator.copy();
+
+                try {
+                    TreeNode node = parser.parse(copiedIterator);
+                    if (copiedIterator.hasNext()) {
+                        continue;
+                    }
+
+                    return node;
+                } catch (NodeFormatException e) {
+                    continue;
+                }
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            Token firstToken = null;
+            while (iterator.hasNext()) {
+                Token token = iterator.next();
+                stringBuilder.append(token.value()).append(" ");
+
+                if (firstToken == null) {
+                    firstToken = token;
+                }
+            }
+
+            throw new NodeFormatException("meaningful tokens", stringBuilder.toString(), firstToken);
+        }
+
+        @NotNull
+        private List<TreeNodeParser<? extends TreeNode>> parsers() {
+            if (parsers == null) {
+                parsers = List.of(
+                        PrimaryNode.PARSER, IdentifierNode.PARSER, ExpressionNode.PARSER, ArgumentsNode.PARSER,
+                        StatementNode.PARSER, ParametersNode.PARSER, ClassMemberNode.PARSER,
+                        ClassNode.PARSER, ProgramNode.PARSER
+                );
+            }
+
+            return parsers;
+        }
+    };
 
 }
